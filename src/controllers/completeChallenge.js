9c1f3cc59356966e448afe21efe271a6d90a1c91
jacekelgda <jacekelgda@gmail.com>
2017-10-23 14:37:10 +0200

    wip

---
+++ src/controllers/completeChallenge.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.submit = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  callback(null, response);
+  });
+};
