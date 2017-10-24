47633265484938e3753181557232d8dc1e5118c4
Jacek Lawniczak <jacekelgda@gmail.com>
2017-10-24 15:23:26 +0200

    Api hints params (#13)
    * Hinting if params were not provided
    
    * Api hints

--- src/controllers/completeChallenge.js
+++ src/controllers/completeChallenge.js
@@ -4,10 +4,49 @@ const Slack = require('slack-node');
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.complete = (event, context, callback) => {
+module.exports.complete = async (event, context, callback) => {
   const data = JSON.parse(event.body);
   const slack = new Slack(process.env.SLACK_API_TOKEN);
   const ts = data.ts;
+
+  if (!data.userId) {
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'We shal not pass this request. We need to know userId of posted message.',
+    });
+    return;
+  }
+
+  const isParticipant = await checkIfParticipant(data.userId);
+  if (!isParticipant) {
+    callback(null, {
+      statusCode: 404,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'We shal not pass this request. Provided userId is not of participant.',
+    });
+    return;
+  }
+
+  if (!data.ts) {
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'We shal not pass this request. We need to know ts of posted message.',
+    });
+    return;
+  }
+
+  const reg = /^.{10}\..{6}$/g;
+  if (reg.test(data.ts)) {
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'We shal not pass this request. Remove that annoying dot from the middle of the string.',
+    });
+    return;
+  }
+
   const messageLink = `User has submitted challenge as done: https://x-team.slack.com/archives/${process.env.MAIN_SLACK_CHANNEL}/p${ts}`;
 
   const attachments = [
@@ -53,7 +92,7 @@ module.exports.complete = (event, context, callback) => {
 
   const response = {
     statusCode: 201,
-    body: 'OK',
+    body: 'Congrats. Your request is valid. It will be decided if The Guardians will approve it.',
   };
 
   callback(null, response);
@@ -101,4 +140,25 @@ module.exports.heal = (event, context, callback) => {
 
     callback(null, response);
   });
+}
+
+const fetchChannelUsers = () => {
+  const slack = new Slack(process.env.SLACK_API_TOKEN);
+  return new Promise((resolve, reject) => {
+    const data = {
+      channel: process.env.MAIN_SLACK_CHANNEL
+    };
+    slack.api('channels.info', data, (err, response) => {
+      if (response.ok === false) {
+          reject(response.error);
+      } else if (response.ok === true) {
+          resolve(response.channel.members);
+      }
+    })
+  })
+}
+
+const checkIfParticipant = async (userId) => {
+  const channelUsers = await fetchChannelUsers();
+  return channelUsers.includes(userId);
 }
