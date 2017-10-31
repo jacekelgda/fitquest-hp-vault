f83d11894e112dcb2b9b8784902277d5d4446812
Jacek Lawniczak <jacekelgda@gmail.com>
2017-10-31 22:25:45 +0100

    Init endp (#18)
    * init endpoint
    
    * init users
    
    * add, remove, list, my hp
    
    * improvements

--- src/controllers/updateHealthProfile.js
+++ src/controllers/updateHealthProfile.js
@@ -77,7 +77,42 @@ module.exports.healcmd = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify('HP increased by +1HP'),
+      body: JSON.stringify(result),
+    };
+
+    callback(null, response);
+  });
+}
+
+module.exports.hitcmd = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  const timestamp = new Date().getTime();
+  const params = {
+    TableName: process.env.HP_TABLE_NAME,
+    Key: {
+      id: data.userId,
+    },
+    ExpressionAttributeValues: {
+      ':value': 1,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET hp = hp - :value, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  dynamoDb.update(params, (error, result) => {
+    if (error) {
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t update the hp profile.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
     };
 
     callback(null, response);
