f2e0fce592dd4ce0ea04ebadbfc0b3f23eaf7616
jacekelgda <jacekelgda@gmail.com>
2017-10-31 10:30:16 +0100

    init endpoint

--- src/controllers/createHealthProfile.js
+++ src/controllers/createHealthProfile.js
@@ -45,4 +45,41 @@ module.exports.create = (event, context, callback) => {
 
     callback(null, response);
   });
-};
+}
+
+module.exports.init = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  for (let i = 0; i < data.length; i += 1) {
+    const params = {
+      TableName: process.env.HP_TABLE_NAME,
+      Item: {
+        id: data[i].slackid,
+        slackid: data[i].slackid,
+        userName: data[i].slackusername,
+        hp: 3,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    };
+
+    dynamoDb.put(params, error => {
+      if (error) {
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Couldn\'t create the HP profile item.',
+        });
+        return;
+      }
+
+      const response = {
+        statusCode: 201,
+        body: JSON.stringify(params.Item),
+      };
+
+      callback(null, response);
+    });
+  }
+}
